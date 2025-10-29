2ee66329b626fe1e9ea446afa5fed5b99f8257af
vandersongc <vandersongc@gmail.com>
2025-10-29 19:35:14 -0300

    v1

---
+++ aplicacao/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home, name='home'),
+    path('produtos/', views.produtos, name='produtos'),
+    path('clientes', views.clientes, name='clientes' )
+]
