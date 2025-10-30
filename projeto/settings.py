efa3b3e7f8580c8c27852025a913bbeab305d6e3
vandersongc <vandersongc@gmail.com>
2025-10-29 21:30:10 -0300

    v4

--- projeto/settings.py
+++ projeto/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
